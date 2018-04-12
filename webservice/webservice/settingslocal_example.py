1f33cf55a802efa0f746095dd46261c7fc32145e
isnochys <isnochys@gmail.com>
2018-04-12 08:26:10 +0200

    start django/webservice
    Basic Django setup

---
+++ webservice/webservice/settingslocal_example.py
@@ -0,0 +1,17 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'supersecretsecretkey_for_django'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
